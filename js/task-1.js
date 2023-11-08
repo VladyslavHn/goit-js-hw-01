7a6a80f4e059a7519510a464b7e82b7b4a9dcbcb
VladyslavHn <vlad.gnatok@gmail.com>
2023-11-08 16:44:47 +0100

    homework-js-01

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+
+function makeTransaction(quantity, pricePerDroid) {
+    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+}
+
+makeTransaction(5, 3000);
+makeTransaction(3, 1000);
+makeTransaction(10, 500);
---
+++ js/task-2.js
@@ -0,0 +1,8 @@
+function getShippingMessage(country, price, deliveryFee) {
+    let totalPrice = price + deliveryFee;
+    return `Shipping to ${country} will cost ${totalPrice} credits`;
+}
+
+getShippingMessage("Australia", 120, 50);
+getShippingMessage("Germany", 80, 20);
+getShippingMessage("Sweden", 100, 20);
